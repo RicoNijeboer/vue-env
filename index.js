dc28f8651b679591585c88b071065ff8177f7538
Rico Nijeboer <r.nijeboer@code14.nl>
2019-07-03 19:30:06 +0200

    Expose env function for usage in .js files

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const environment = process.env;
 const existingKeys = Object.keys(environment);
 
+export function env(key, defaultValue) {
+  if (existingKeys.includes(key)) {
+    return environment[key];
+  }
+
+  const vueAppKey = `VUE_APP_${key}`;
+
+  if (existingKeys.includes(vueAppKey)) {
+    return environment[vueAppKey];
+  }
+
+  return defaultValue;
+}
+
 export default {
   install(Vue) {
-    Vue.prototype.$env = (key, defaultValue) => {
-      if (existingKeys.includes(key)) {
-        return environment[key];
-      }
-
-      const vueAppKey = `VUE_APP_${key}`;
-
-      if (existingKeys.includes(vueAppKey)) {
-        return environment[vueAppKey];
-      }
-
-      return defaultValue;
-    };
-  }
+    Vue.prototype.$env = env;
+  },
 };
